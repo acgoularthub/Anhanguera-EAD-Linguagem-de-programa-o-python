523796581fdaed83b3a49292344b2c929cceaf26
Anderson Coelho Goulart <acgoulartmail@gmail.com>
2020-10-05 09:06:08 -0300

    Carregando np de uso de graficos da biblioteca seaborn

---
+++ aula 04/manipulando-csv.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+#Gerando numeros
+x = np.random.randn(200)
+y = np.random.randn(200)
+
+#definindo dataframe
+df = pd.DataFrame({'x' : x , 'y' : y })
+
+#criando tabela de distribuição
+sns.displot(df['x'])
+plt.show()
+
+#criando grafico de barras 
+sns.barplot(df.x, df.y)
+plt.show()
+
+#cria um "grafico" plotando a relação de x com y
+sns.scatterplot(df.x, df.y, hue = df.x)
